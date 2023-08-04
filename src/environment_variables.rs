33196c522eaafb529e83e9fbb819c84f359ac07f
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2023-08-04 13:58:42 +0200

    Try to connect to kafka avien with ssl

--- src/environment_variables.rs
+++ src/environment_variables.rs
@@ -1,15 +1,40 @@
 use std::env;
-use crate::EnvironmentVariables;
+use serde_derive::{Deserialize, Serialize};
 
 const INTERN_PIK_TOPIC: &str = "teamsykmelding.paragraf-i-kode";
 const ETTERLEVELSE_TOPIC: &str = "flex.omrade-helse-etterlevelse";
 
 pub fn get_environment_variables()  -> EnvironmentVariables {
     let cluster_name: String = env::var("NAIS_CLUSTER_NAME").unwrap_or("localhost".to_string());
+    let application_name: String = env::var("NAIS_APP_NAME").unwrap_or("environment variable NAIS_APP_NAME is missing".to_string());
+    let kafka_brokers: String= env::var("KAFKA_BROKERS").unwrap_or("environment variable KAFKA_BROKERS is missing".to_string());
+    let kafka_hostname: String= env::var("HOSTNAME").unwrap_or("environment variable HOSTNAME is missing".to_string());
+    let kafka_certificate_path: String= env::var("kafka_certificate_path").unwrap_or("environment variable kafka_certificate_path is missing".to_string());
+    let kafka_ca_path: String= env::var("KAFKA_CA_PATH").unwrap_or("environment variable KAFKA_CA_PATH is missing".to_string());
+    let kafka_private_key_path : String= env::var("KAFKA_PRIVATE_KEY_PATH").unwrap_or("environment variable KAFKA_PRIVATE_KEY_PATH is missing".to_string());
 
     return EnvironmentVariables {
         intern_pik_topic: INTERN_PIK_TOPIC.to_string(),
         etterlevelse_topic: ETTERLEVELSE_TOPIC.to_string(),
         cluster_name,
+        application_name,
+        kafka_brokers,
+        kafka_hostname,
+        kafka_certificate_path,
+        kafka_ca_path,
+        kafka_private_key_path
     };
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct EnvironmentVariables {
+    pub(crate) intern_pik_topic: String,
+    pub(crate) etterlevelse_topic: String,
+    pub(crate) cluster_name: String,
+    pub(crate) application_name: String,
+    pub(crate) kafka_brokers: String,
+    pub(crate) kafka_hostname: String,
+    pub(crate) kafka_certificate_path: String,
+    pub(crate) kafka_ca_path: String,
+    pub(crate) kafka_private_key_path: String
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 mod handle_client;
 mod environment_variables;
 
+use std::fs::File;
 use std::net::{TcpListener};
 use std::string::ToString;
+use kafka::client::{KafkaClient, SecurityConfig};
 use serde_derive::{Deserialize, Serialize};
 use crate::environment_variables::get_environment_variables;
 use crate::handle_client::handle_client;
-
+use kafka::consumer::{Consumer, FetchOffset};
+use openssl::ssl::{SslConnector, SslFiletype, SslMethod, SslVerifyMode};
 
 
 fn main() {
@@ -21,7 +24,7 @@ fn main() {
 
     let application_state = ApplicationState {
         alive: true,
-        ready: true
+        ready: true,
     };
 
     for stream in listener.incoming() {
@@ -34,17 +37,63 @@ fn main() {
             }
         }
     }
+
+    // setup ssl config for kafka aiven
+    let kafka_brokers = environment_variables.kafka_brokers.to_string();
+    let kafka_certificate_path = environment_variables.kafka_certificate_path.to_string();
+    let kafka_private_key_path = environment_variables.kafka_private_key_path.to_string();
+    let kafka_ca_path = environment_variables.kafka_ca_path.to_string();
+
+    let certificate_file = File::open(kafka_certificate_path);
+    let private_key_file = File::open(kafka_private_key_path);
+    let ca_file = File::open(kafka_ca_path);
+
+    let mut ssl_connector_builder = SslConnector::builder(SslMethod::tls()).unwrap();
+    ssl_connector_builder.set_cipher_list("DEFAULT").unwrap();
+    ssl_connector_builder.set_verify(SslVerifyMode::PEER).unwrap();
+    ssl_connector_builder
+        .set_certificate_file(certificate_file, SslFiletype::PEM)
+        .unwrap();
+    ssl_connector_builder
+        .set_private_key_file(private_key_file, SslFiletype::PEM)
+        .unwrap();
+    ssl_connector_builder.set_ca_file(ca_file).unwrap();
+
+
+    let ssl_connector = ssl_connector_builder.build();
+
+    let kafka_client: KafkaClient = KafkaClient::new_secure(
+        vec!(kafka_brokers.to_owned(), &[0, 1], ),
+        (SecurityConfig::new(ssl_connector).with_hostname_verification(true)));
+
+    // kafka config
+    let intern_pik_topic = environment_variables.intern_pik_topic.to_string();
+    let kafka_hostname = environment_variables.kafka_hostname.to_string() + "-paragraf-i-kode";
+    let application_name = environment_variables.application_name.to_string();
+
+    // start to consume kafka messeges
+    let mut kafka_consumer =
+        Consumer::from_client(kafka_client)
+            .with_fallback_offset(FetchOffset::Latest)
+            .with_topic_partitions(intern_pik_topic.to_owned(), &[0, 1])
+            .with_group(application_name.to_owned())
+            .with_client_id(kafka_hostname)
+            .create()
+            .unwrap();
+
+    loop {
+        for messageSet in kafka_consumer.poll().unwrap().iter() {
+            for message in messageSet.messages() {
+                println!("{:?}", message);
+            }
+            kafka_consumer.consume_messageset(messageSet).expect("panic message");
+        }
+        kafka_consumer.commit_consumed().unwrap();
+    }
 }
 
 #[derive(Serialize, Deserialize, Clone, Copy)]
 pub struct ApplicationState {
     alive: bool,
     ready: bool,
-}
-
-#[derive(Serialize, Deserialize)]
-pub struct EnvironmentVariables {
-    intern_pik_topic: String,
-    etterlevelse_topic: String,
-    cluster_name: String
 }
